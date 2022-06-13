2862e4cc94d5ffe77855439c6f7843c1f29bec58
Prince Odia <princeodia@gmail.com>
2022-06-13 02:15:26 +0100

    Implement method to add note

--- app/src/main/java/com/princeoprince/notekeeper/DataManager.kt
+++ app/src/main/java/com/princeoprince/notekeeper/DataManager.kt
@@ -10,7 +10,9 @@ object DataManager {
     }
 
     fun addNote(course: CourseInfo, noteTitle: String, noteText: String): Int {
-        return 0
+        val note = NoteInfo(course, noteTitle, noteText)
+        notes.add(note)
+        return notes.lastIndex
     }
 
     private fun initialiseCourses() {
