803dae477d210586e03d36213a30e1e4f45e73b7
Prince Odia <princeodia@gmail.com>
2022-06-13 02:29:10 +0100

    Implement method to find note

--- app/src/main/java/com/princeoprince/notekeeper/DataManager.kt
+++ app/src/main/java/com/princeoprince/notekeeper/DataManager.kt
@@ -15,6 +15,14 @@ object DataManager {
         return notes.lastIndex
     }
 
+    fun findNote(course: CourseInfo, noteTitle: String, noteText: String): NoteInfo? {
+        for (note in notes) {
+            if (course == note.course && noteTitle == note.title && noteText == note.text)
+                return note
+        }
+        return null
+    }
+
     private fun initialiseCourses() {
         var course = CourseInfo("android_intents", "Android Programming with Intents")
         courses[course.courseId] = course
