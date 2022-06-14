f2691f72bb6d78a1cf4b22f1c4df6b6bf9e2df54
Prince Odia <princeodia@gmail.com>
2022-06-14 20:40:44 +0100

    Set up UI test to create a new note

---
+++ app/src/androidTest/java/com/princeoprince/notekeeper/CreateNewNoteTest.kt
@@ -0,0 +1,15 @@
+package com.princeoprince.notekeeper
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class CreateNewNoteTest{
+
+    @Test
+    fun createNewNote() {
+
+    }
+}
