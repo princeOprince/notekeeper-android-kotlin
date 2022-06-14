5d855f0f2da3e11d72ff618f786647a73d47a73e
Prince Odia <princeodia@gmail.com>
2022-06-14 21:05:35 +0100

    Add required imports for automated UI test

--- app/src/androidTest/java/com/princeoprince/notekeeper/CreateNewNoteTest.kt
+++ app/src/androidTest/java/com/princeoprince/notekeeper/CreateNewNoteTest.kt
@@ -1,9 +1,15 @@
 package com.princeoprince.notekeeper
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
+import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import org.hamcrest.Matchers.*
+import androidx.test.espresso.action.ViewActions.*
+import org.junit.Rule
 
 @RunWith(AndroidJUnit4::class)
 class CreateNewNoteTest{
