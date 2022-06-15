7eadfb56e4eaf77775963e59445dedbbfc7ea6b0
Prince Odia <princeodia@gmail.com>
2022-06-15 03:39:03 +0100

    Implement spinner and back button interactions

--- app/src/androidTest/java/com/princeoprince/notekeeper/CreateNewNoteTest.kt
+++ app/src/androidTest/java/com/princeoprince/notekeeper/CreateNewNoteTest.kt
@@ -10,6 +10,7 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import org.hamcrest.Matchers.*
 import androidx.test.espresso.action.ViewActions.*
 import org.junit.Rule
+import androidx.test.espresso.Espresso.pressBack
 
 @RunWith(AndroidJUnit4::class)
 class CreateNewNoteTest{
@@ -19,12 +20,18 @@ class CreateNewNoteTest{
 
     @Test
     fun createNewNote() {
+        val course = DataManager.courses["android_async"]
         val noteTitle = "Test note title"
         val noteText = "This is the body of our test note"
 
         onView(withId(R.id.fab)).perform(click())
 
+        onView(withId(R.id.spinnerCourses)).perform(click())
+        onData(allOf(instanceOf(CourseInfo::class.java), equalTo(course))).perform(click())
+
         onView(withId(R.id.textNoteTitle)).perform(typeText(noteTitle))
         onView(withId(R.id.textNoteText)).perform(typeText(noteText))
+
+        pressBack()
     }
 }
