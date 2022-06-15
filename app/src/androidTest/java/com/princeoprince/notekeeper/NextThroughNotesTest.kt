c26413747b3b0e3770e9a5667b0e7cfe6411d4af
Prince Odia <princeodia@gmail.com>
2022-06-15 04:06:28 +0100

    Create class to test menu next ui behaviour

---
+++ app/src/androidTest/java/com/princeoprince/notekeeper/NextThroughNotesTest.kt
@@ -0,0 +1,25 @@
+package com.princeoprince.notekeeper
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import org.hamcrest.Matchers.*
+import androidx.test.espresso.action.ViewActions.*
+import org.junit.Rule
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+
+@RunWith(AndroidJUnit4::class)
+class NextThroughNotesTest{
+
+    @Rule @JvmField
+    val noteListActivity = ActivityScenarioRule(NoteListActivity::class.java)
+
+    @Test
+    fun nextThroughNotes() {
+    }
+}
