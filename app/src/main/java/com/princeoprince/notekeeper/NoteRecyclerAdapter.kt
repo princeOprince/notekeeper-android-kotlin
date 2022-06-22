4a2b5dd505ebc3d0473d586656b634e7ab83e043
Prince Odia <princeodia@gmail.com>
2022-06-22 02:25:50 +0100

    Create the RecyclerView Adapter

---
+++ app/src/main/java/com/princeoprince/notekeeper/NoteRecyclerAdapter.kt
@@ -0,0 +1,27 @@
+package com.princeoprince.notekeeper
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteRecyclerAdapter(private val context: Context) :
+    RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>() {
+
+    private val layoutInflater = LayoutInflater.from(context)
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
