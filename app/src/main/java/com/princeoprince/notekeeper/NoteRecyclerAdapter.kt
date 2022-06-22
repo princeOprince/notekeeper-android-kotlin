6054eb310703ba5f09a33c17769c5139efa39c6a
Prince Odia <princeodia@gmail.com>
2022-06-22 03:08:37 +0100

    Manage data within the RecyclerView Adapter

--- app/src/main/java/com/princeoprince/notekeeper/NoteRecyclerAdapter.kt
+++ app/src/main/java/com/princeoprince/notekeeper/NoteRecyclerAdapter.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class NoteRecyclerAdapter(private val context: Context) :
+class NoteRecyclerAdapter(private val context: Context, private val notes: List<NoteInfo>) :
     RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>() {
 
     private val layoutInflater = LayoutInflater.from(context)
@@ -23,10 +23,10 @@ class NoteRecyclerAdapter(private val context: Context) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val note = notes[position]
+        holder.texCourse.text = note.course?.title
+        holder.textTitle.text = note.title
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = notes.size
 }
