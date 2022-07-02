7b111741179e0e28850ead23298b6c24255aba73
Prince Odia <princeodia@gmail.com>
2022-07-02 09:18:46 +0100

    Implement automated test on course selection

--- app/src/androidTest/java/com/princeoprince/notekeeper/NavigationTest.kt
+++ app/src/androidTest/java/com/princeoprince/notekeeper/NavigationTest.kt
@@ -21,6 +21,14 @@ class NavigationTest {
 
     @Test
     fun selectNoteAfterNavigationDrawerChange() {
+      onView(withId(R.id.drawer_layout)).perform(DrawerActions.open())
+      onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_courses))
 
+      val coursePosition = 0
+      onView(withId(R.id.listItems)).perform(
+          RecyclerViewActions
+              .actionOnItemAtPosition<CourseRecyclerAdapter.ViewHolder>(
+                  coursePosition, click())
+      )
     }
 }
