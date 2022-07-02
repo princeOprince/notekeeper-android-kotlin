e14085387f74af4aaf0a76d7480d58e2064fedc8
Prince Odia <princeodia@gmail.com>
2022-07-02 03:52:14 +0100

    Set up test for navigation

---
+++ app/src/androidTest/java/com/princeoprince/notekeeper/NavigationTest.kt
@@ -0,0 +1,23 @@
+package com.princeoprince.notekeeper
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Test
+import org.junit.runner.RunWith
+import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import org.hamcrest.Matchers.*
+import androidx.test.espresso.action.ViewActions.*
+import org.junit.Rule
+import androidx.test.espresso.assertion.ViewAssertions.matches
+
+@RunWith(AndroidJUnit4::class)
+class NavigationTest {
+    @Rule @JvmField
+    val itemsActivity = ActivityScenarioRule(ItemsActivity::class.java)
+
+    @Test
+    fun selectNoteAfterNavigationDrawerChange() {
+
+    }
+}
