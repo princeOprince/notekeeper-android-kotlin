009008299cbee12627b926a3d33f8517c6f87eb5
Prince Odia <princeodia@gmail.com>
2022-07-08 00:19:27 +0100

    Add noteId functionality to DataManager

--- app/src/main/java/com/princeoprince/notekeeper/DataManager.kt
+++ app/src/main/java/com/princeoprince/notekeeper/DataManager.kt
@@ -9,11 +9,37 @@ object DataManager {
         initialiseNotes()
     }
 
+    fun loadNotes(vararg noteIds: Int): List<NoteInfo> {
+        val noteList: List<NoteInfo>
+
+        if (noteIds.isEmpty())
+            noteList = notes
+        else {
+            noteList = ArrayList<NoteInfo>(noteIds.size)
+            for (noteId in noteIds)
+                noteList.add(notes[noteId])
+        }
+        return noteList
+    }
+
+    fun loadNote(noteId: Int) = notes[noteId]
+
+    fun isLastNoteId(noteId: Int) = noteId == notes.lastIndex
+
+    private fun idOfNote(note: NoteInfo) = notes.indexOf(note)
+
+    fun noteIdsAsIntArray(notes: List<NoteInfo>): IntArray {
+        val noteIds = IntArray(notes.size)
+        for (index in 0..notes.lastIndex)
+            noteIds[index] = idOfNote(notes[index])
+        return noteIds
+    }
+
     fun addNote(course: CourseInfo, noteTitle: String, noteText: String): Int {
         return addNote(NoteInfo(course, noteTitle, noteText))
     }
 
-    fun addNote(note: NoteInfo): Int {
+    private fun addNote(note: NoteInfo): Int {
         notes.add(note)
         return notes.lastIndex
     }
