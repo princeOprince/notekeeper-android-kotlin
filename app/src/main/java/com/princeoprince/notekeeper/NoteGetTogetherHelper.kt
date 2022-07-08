af2d2a73aacfc5ab09d5f346011d620cdc072919
Prince Odia <princeodia@gmail.com>
2022-07-08 17:54:06 +0100

    Observe lifecycle events

--- app/src/main/java/com/princeoprince/notekeeper/NoteGetTogetherHelper.kt
+++ app/src/main/java/com/princeoprince/notekeeper/NoteGetTogetherHelper.kt
@@ -2,17 +2,33 @@ package com.princeoprince.notekeeper
 
 import android.content.Context
 import android.util.Log
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 
-class NoteGetTogetherHelper(val context: Context): LifecycleObserver {
+class NoteGetTogetherHelper(val context: Context, private val lifecycle: Lifecycle): LifecycleObserver {
 
-    val tag = this::class.simpleName
-    var currentLat = 0.0
-    var currentLon = 0.0
+    init {
+        lifecycle.addObserver(this)
+    }
 
-    val locManager = PseudoLocationManager(context) { lat, lon ->
+    private val tag = this::class.simpleName
+    private var currentLat = 0.0
+    private var currentLon = 0.0
+
+    private val locManager = PseudoLocationManager(context) { lat, lon ->
         currentLat = lat
         currentLon = lon
         Log.d(tag, "Location Callback Lat:$currentLat Lon:$currentLon")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun startHandler() {
+        locManager.start()
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun stopHandler() {
+        locManager.stop()
     }
 }
